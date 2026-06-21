213bedb79b3b580932a3e07dad67a18dbe78a139
Ahmad <kkineticcbtw@gmail.com>
2026-06-21 19:50:48 +0100

    imperial feature

--- main.py
+++ main.py
@@ -20,3 +20,25 @@ if branch == "chelsea":
     print("chelsea is the best team in the world")
 else:
     print("chelsea is not the best team in the world")
+
+# Feature: Display top 10 UK Universities
+def get_top_10_uk_universities():
+    """Returns the current top 10 universities in the UK based on QS Rankings"""
+    top_10_unis = [
+        "University of Oxford",
+        "University of Cambridge",
+        "Imperial College London",
+        "University College London (UCL)",
+        "London School of Economics (LSE)",
+        "University of Manchester",
+        "University of Edinburgh",
+        "University of Warwick",
+        "Durham University",
+        "University of Bath"
+    ]
+    return top_10_unis
+
+print("\n--- Top 10 Universities in the UK ---")
+universities = get_top_10_uk_universities()
+for i, uni in enumerate(universities, 1):
+    print(f"{i}. {uni}")
